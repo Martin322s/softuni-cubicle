a9ffa59e64a1b80a1daeefe0556d09ec5889873f
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-05-12 11:21:07 +0300

    add start script

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ const { initialDatabase } = require('../config/database');
 const app = express();
 const port = process.env.PORT || 5500;
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 initialDatabase()
     .then(() => {
-        app.listen(port, () => console.log(`Server is working at: ${port}...`));
+        console.log("Database initialized successfully!");
+        app.listen(port, () => console.log(`Server is working at: http://localhost:${port}`));
     })
     .catch(err => {
         console.log(`Database error: ${err}`);
