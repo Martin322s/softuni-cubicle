0cb60b0debbab105c8379e89cc0e88e93de3e0fe
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-05-13 14:25:50 +0300

    render details page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,6 +12,8 @@ router.get('/create', (req, res) => {
     res.render('create');
 });
 
-
+router.get('/details/:cubeId', (req, res) => {
+    res.render('details');
+});
 
 module.exports = router;
