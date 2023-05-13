a0f7088f3717e5cecb61e2e64a7bac77000947d4
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-05-13 15:47:56 +0300

    model for cubes

---
+++ src/models/Cube.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true
+    }
+});
+
+const Cube = mongoose.model('Cube', cubeSchema);
+
+module.exports = Cube;
