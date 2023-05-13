001f759bd6c0cd870357f4c3c362f0fbbad3cfa5
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-05-13 14:18:57 +0300

    get all static assets

--- config/handlebars.js
+++ config/handlebars.js
@@ -3,5 +3,5 @@ const hbs = require('express-handlebars');
 exports.initViewEngine = (app) => {
     app.engine('hbs', hbs.engine({ extname: 'hbs' }));
     app.set('view engine', 'hbs');
-    app.set('views', '../src/views');
+    app.set('views', './src/views');
 };
