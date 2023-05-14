aed23d717ccc230de9e8c9f31db0b381123d383a
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-05-14 18:59:58 +0300

    render register page

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const authController = require('./controllers/authController');
 
 router.use('/', homeController);
 router.use('/cube', cubeController);
+router.use('/users', authController);
 router.get('*', (req, res) => {
     res.render('404');
 });
