7f79053e20b6d58b39a108f13fc2d689ad84216e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-05-14 13:31:59 +0300

    search by difficulty level

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,14 +10,18 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const { search, from, to } = req.body;
     let cubes = await getAll();
-    console.log(search, from, to);
 
     if (search) {
         cubes = cubes.filter(x => x.name.toLowerCase().includes(search?.toLowerCase()));
     }
 
-    
-    res.render('home', { cubes, search });
+    if (from) {
+        cubes = cubes.filter(x => x.difficultyLevel >= Number(from));
+    }
+    if (to) {
+        cubes = cubes.filter(x => x.difficultyLevel <= Number(to));
+    }
+    res.render('home', { cubes, search, from, to });
 });
 
 module.exports = router;
